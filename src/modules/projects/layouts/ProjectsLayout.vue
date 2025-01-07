efc623cf08ad8dfb0a1e431d85001c97f6a40345
chaicopadillag <xt9738@rimac.com.pe>
2025-01-07 11:22:26 -0500

    add common components and layout for projects view

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col">
+    <TopMenu />
+    <main class="flex flex-row">
+      <SideMenu />
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+</script>
---
+++ src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="overflow-x-auto w-full">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        <tr>
+          <th>1</th>
+          <td>Cy Ganderton</td>
+          <td>Quality Control Specialist</td>
+          <td>Blue</td>
+        </tr>
+      </tbody>
+    </table>
+    <FabButton @click="() => console.log('Emit click')"> <AddCircle /> </FabButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import FabButton from '@/modules/common/components/FabButton.vue';
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+</script>
