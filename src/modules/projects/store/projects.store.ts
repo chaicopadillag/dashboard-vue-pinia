7193b84f61bf995d1bbc65c88cc084551b0c9dea
chaicopadillag <xt9738@rimac.com.pe>
2025-01-07 20:09:29 -0500

    add @vueuse/core dependency and refactor projects store to use local storage

--- src/modules/projects/store/projects.store.ts
+++ src/modules/projects/store/projects.store.ts
@@ -1,46 +1,11 @@
+import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
 import { v4 as uuid } from 'uuid';
 import { computed, ref } from 'vue';
-import type { ProjectType } from '../types/projects.interface';
+import type { ProjectType } from '../interfaces/projects.interface';
 
 export const useProjectsStore = defineStore('projectsStore', () => {
-  const projects = ref<ProjectType[]>([
-    {
-      id: uuid(),
-      name: 'Project 1',
-      description: 'Description for project 1',
-      tasks: [
-        {
-          id: uuid(),
-          name: 'Task 1',
-          description: 'Description for task 1',
-          completedAt: new Date(),
-        },
-        {
-          id: uuid(),
-          name: 'Task 2',
-          description: 'Description for task 2',
-        },
-      ],
-    },
-    {
-      id: uuid(),
-      name: 'Project 2',
-      description: 'Description for project 2',
-      tasks: [
-        {
-          id: uuid(),
-          name: 'Task 1',
-          description: 'Description for task 1',
-        },
-        {
-          id: uuid(),
-          name: 'Task 2',
-          description: 'Description for task 2',
-        },
-      ],
-    },
-  ]);
+  const projects = ref(useLocalStorage<ProjectType[]>('projects', []));
 
   const addNewProject = (name: string) => {
     projects.value.push({
