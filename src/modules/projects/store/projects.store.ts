c8eb2e30a29dcb13c5d955b173a02d90d44a5c99
chaicopadillag <xt9738@rimac.com.pe>
2025-01-09 11:58:12 -0500

    feat: add project task metrics and toggle task completion functionality

--- src/modules/projects/store/projects.store.ts
+++ src/modules/projects/store/projects.store.ts
@@ -11,6 +11,21 @@ export const useProjectsStore = defineStore('projectsStore', () => {
   const noProjects = computed(() => projectList.value.length === 0);
   const findProjectById = (id: string) => projects.value.find((project) => project.id === id);
 
+  const projectTaskMetrics = computed(() =>
+    projects.value.map((project) => {
+      const totalTasks = project.tasks.length;
+      const completedTasks = project.tasks.filter((task) => task.completedAt).length;
+      const completionRate = totalTasks === 0 ? 0 : (completedTasks / totalTasks) * 100;
+      return {
+        id: project.id,
+        name: project.name,
+        completedTasks,
+        totalTasks,
+        completionRate: Math.round(completionRate),
+      };
+    }),
+  );
+
   const addNewProject = (name: string) => {
     projectList.value.push({
       id: uuid(),
@@ -29,11 +44,23 @@ export const useProjectsStore = defineStore('projectsStore', () => {
     }
   };
 
+  const toggleCompletedTask = (projectId: string, taskId: string) => {
+    const project = projectList.value.find((project) => project.id === projectId);
+    if (project) {
+      const task = project.tasks.find((task) => task.id === taskId);
+      if (task) {
+        task['completedAt'] = task['completedAt'] ? undefined : new Date();
+      }
+    }
+  };
+
   return {
     projects,
+    projectTaskMetrics,
     noProjects,
     findProjectById,
     addNewProject,
     addNewTask,
+    toggleCompletedTask,
   };
 });
