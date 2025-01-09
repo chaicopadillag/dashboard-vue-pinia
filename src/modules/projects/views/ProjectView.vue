e079de361f6052d0f7054b79503ee579149e0681
chaicopadillag <xt9738@rimac.com.pe>
2025-01-09 09:06:07 -0500

    add router navigation fallback in ProjectView for missing projects

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -5,9 +5,11 @@
 <script lang="ts" setup>
 import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 import type { ProjectType } from '../interfaces/projects.interface';
 import { useProjectsStore } from '../store/projects.store';
 
+const router = useRouter();
 const props = defineProps<{ id: string }>();
 const project = ref<ProjectType | undefined>(undefined);
 const store = useProjectsStore();
@@ -16,6 +18,9 @@ watch(
   () => props.id,
   (id) => {
     project.value = store.findProjectById(id);
+    if (!project.value) {
+      router.replace('/');
+    }
   },
   {
     immediate: true,
